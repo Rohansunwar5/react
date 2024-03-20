1aee2bb1dd2b06314cb5e16c1e0bddb9e0f4c17c
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-20 21:14:37 +0530

    add setup components

--- mega blog/src/App.jsx
+++ mega blog/src/App.jsx
@@ -1,13 +1,40 @@
+import { useDispatch } from 'react-redux';
 import './App.css'
+import { useState } from 'react';
+import authService from './appwrite/auth'
+import { useEffect } from 'react';
+import {login, logout} from './store/authSlice'
+import { Outlet } from 'react-router-dom';
+import {Header, Footer} from './components/index.js'
 
 function App() {
-  
-  // console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <main>
-    Hellow 
-    </main>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser() //asking weather the current  user is logged in or not
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[]);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* Todo */}
+          {/* <Outlet /> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
