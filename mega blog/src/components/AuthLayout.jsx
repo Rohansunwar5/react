6af83b29ce5b37f774b43b4bc82bef59d3355cbd
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-23 19:41:08 +0530

    add update components

---
+++ mega blog/src/components/AuthLayout.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { UseSelector, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function Protected({
+  children,
+  authentication = true
+}) {
+  const navigate = useNavigate()
+  const [loader, setLoader] = useState(true)
+  const authStatus = useSelector(state => state.auth.status)
+
+  useEffect(() => {
+
+    //TODO: make easy
+    if(authentication && authStatus !== authentication){
+      navigate('/login')
+    } 
+    else if (!authentication && authStatus !== authentication){
+      navigate('/')
+    }
+    setLoader(false)
+  }, [authStatus, navigate, authentication])
+
+  return (
+    loader ? <h1>Loading....</h1> : 
+    <>
+    {children}
+    </>
+  )
+}
+
+
--- mega blog/src/components/index.js
+++ mega blog/src/components/index.js
@@ -3,8 +3,12 @@ import Footer from "./Footer/Footer.jsx"
 import Container from "./container/Container.jsx"
 import Logo from "./Logo.jsx"
 import LogoutBtn from "./Header/LogoutBtn.jsx"
+import Input from "./Input.jsx"
+import Button from './Button.jsx'
 
 export {
+  Button,
+  Input,
   Header,
   Footer,
   Container,
