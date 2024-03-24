fe6efc5be60b8a1caae893aa4e00072845a6e0b6
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-25 01:11:31 +0530

    add page setup

--- mega blog/src/components/index.js
+++ mega blog/src/components/index.js
@@ -6,8 +6,20 @@ import LogoutBtn from "./Header/LogoutBtn.jsx"
 import Input from "./Input.jsx"
 import Button from './Button.jsx'
 import RTE from "./RTE.jsx"
+import Signup from './Signup.jsx'
+import Login from './Login.jsx'
+import PostForm from '../components/post-form/PostForm.jsx'
+import PostCard from './PostCard.jsx'
+import AuthLayout from './AuthLayout.jsx'
+import Select from './Select.jsx'
 
 export {
+  Select,
+  AuthLayout,
+  PostCard,
+  PostForm,
+  Login,
+  Signup,
   RTE,
   Button,
   Input,
